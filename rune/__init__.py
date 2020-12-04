e6c216371395013fe2123c71039afb7ff14ed6cc
Horia Filimonescu <horia.filimonescu+github@gmail.com>
2020-12-04 11:05:20 +0100

    * Bump version number to 0.3.0

--- rune/__init__.py
+++ rune/__init__.py
@@ -27,7 +27,7 @@ from sqlalchemy import MetaData
 
 from .config import Config
 
-__version__ = '0.2.2.dev'
+__version__ = '0.3.0'
 
 
 # https://stackoverflow.com/a/46785675
--- rune_theme/__init__.py
+++ rune_theme/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 
-__version__ = '0.2.2.dev'
+__version__ = '0.3.0'
 
 
 class Theme:
