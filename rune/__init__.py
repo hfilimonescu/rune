cb65f704450238fc19b8b8a71f678eb5468e6472
Horia Filimonescu <horia.filimonescu+github@gmail.com>
2020-12-07 12:55:51 +0100

    Start development for 0.3.2.dev

--- rune/__init__.py
+++ rune/__init__.py
@@ -27,7 +27,7 @@ from sqlalchemy import MetaData
 
 from .config import Config
 
-__version__ = '0.3.1'
+__version__ = '0.3.2.dev'
 
 
 # https://stackoverflow.com/a/46785675
--- rune_auth/__init__.py
+++ rune_auth/__init__.py
@@ -8,7 +8,7 @@ login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message_category = 'info'
 
-__version__ = '0.3.1'
+__version__ = '0.3.2.dev'
 
 
 class Auth:
